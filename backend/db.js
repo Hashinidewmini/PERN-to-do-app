042798512e9ebc4d20fc89b8f6f7ef37ef85f6f4
Hashinidewmini <hashinifdo04@gmail.com>
2026-06-07 05:03:25 +0530

    Created .env file

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,14 @@
-import {Pool} from 'pg';
+import { Pool } from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const pool = new Pool({
-    user: "postgres",
-    password: "postgres",
-    host: "localhost",
-    port: 5432,
-    database: "tododb"
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME
 });
 
-export default pool; //to use in routes need to export the pool object so that it can be imported 
-                     // in other files where database operations are performed.
+export default pool;
